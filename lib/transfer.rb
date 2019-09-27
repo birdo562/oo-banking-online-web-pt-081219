555815a8fb2ba8245835df6161e9fe6b1c6f9ab8
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 08:30:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
      end
    end
    def reject_transfer
-     self.status = "rejected"
+     self.status= "rejected"
      "Transaction rejected. Please check your account balance"
   end
 end
