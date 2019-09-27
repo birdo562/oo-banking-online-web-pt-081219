43da8c438e7d00b090ee484a339f2fca94ffa5d0
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 08:09:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -11,7 +11,7 @@ class BankAccount
     @balance += amount
   end
   def display_balance
-    "Your balance is #{@balance}."
+    "Your balance is $#{@balance}."
   end
   def valid?
     if @status == "open" && @balance > 1000
